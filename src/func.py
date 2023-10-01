0d2f7059ac1c97455e8f204fa60e8c463ee5f801
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-10-01 21:34:23 +0800

    added load_stat to func, docstrings

--- src/func.py
+++ src/func.py
@@ -14,11 +14,12 @@ def load_executed(file):
     """filters list for records with Executed status"""
     executed_list = []
     for f in file:
-        if f["state"] == "EXECUTED":
-            executed_list.append(f)
+        if "state" not in f.keys():
+            continue
+        else:
+            if f["state"] == "EXECUTED":
+                executed_list.append(f)
     return executed_list
-
-tl = [{"id": 4949, "date": "2019-08-26T10:50:58.294041"}, {"id": 4969, "date": "2018-08-26T10:50:58.294041"},{"id": 4049, "date": "2020-08-26T10:50:58.294041"}]
 
 def sort_by_date(raw_data):
     """sort the list by the date of a transaction in reverse order"""
@@ -26,14 +27,17 @@ def sort_by_date(raw_data):
 
 def five_recent_trns(tr_list):
     """takes slice of five first items in list"""
-    return five_trns[:5]
+    return tr_list[:5]
 
 def print_stats(filted_list):
+    """print information for user about five recent transactions
+    with date, type of transaction, from, to, amount and currency"""
     for i in filted_list:
         date = datetime.strptime(i["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
         description = i["description"]
         if "from" not in i.keys():
-            continue
+            num_from = ""
+            name_from = ""
         else:
             from_sep = i["from"].split()
             num_list = []
@@ -48,26 +52,27 @@ def print_stats(filted_list):
                 num_from = " ".join([card_num[:4], card_num[4:8], card_num[8:12], card_num[12:]])
             else:
                 num_from = num_list[0].replace(num_list[0][:-4], "**")
-            name_from = "".join(word_list)
+            name_from = " ".join(word_list)
         to_sep = i["to"].split()
         to_num_list = []
         to_word_list = []
-        for d in to_sep:
-            if d.isnumeric():
-                to_num_list.append(d)
+        for t in to_sep:
+            if t.isnumeric():
+                to_num_list.append(t)
             else:
-                to_word_list.append(d)
-            if len(to_num_list[0]) == 16:
-                to_card_num = to_num_list[0]. replace(to_num_list[0][6:12], "******")
-                num_to = " ".join([to_card_num[:4], to_card_num[4:8], to_card_num[8:12], to_card_num[12:]])
-            else:
-                num_to = to_num_list[0].replace(to_num_list[0][:-4], "**")
-            name_to = "".join(to_word_list)
+                to_word_list.append(t)
+        if len(to_num_list[0]) != 16:
+            num_to = to_num_list[0].replace(to_num_list[0][:-4], "**")
+        else:
+            to_card_num = to_num_list[0]. replace(to_num_list[0][6:12], "******")
+            num_to = " ".join([to_card_num[:4], to_card_num[4:8], to_card_num[8:12], to_card_num[12:]])
+        name_to = " ".join(to_word_list)
         amount = i["operationAmount"]["amount"]
         currency = i["operationAmount"]["currency"]["name"]
-        print(f"{date}, {description}"
-              f"{name_from}, {num_from} => {name_to}, {num_to}"
+        print(f"{date}, {description}\n"
+              f"{name_from} {num_from} => {name_to} {num_to}\n"
               f"{amount}, {currency}\n")
+
 
 
 
