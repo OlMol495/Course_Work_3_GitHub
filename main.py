958a78c8621b356bfc4c0b66dafb3fae062d11aa
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-10-01 21:34:43 +0800

    added main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-# def main()
-#     pass
-#
-# if __name__="__main__":
-#     return main()
+from src import func
+
+json_path = "operations.json"
+def main():
+    transaction_list = func.load_data(json_path)
+    exec_filtered = func.load_executed(transaction_list)
+    date_sorted = func.sort_by_date(exec_filtered)
+    recent_five = func.five_recent_trns(date_sorted)
+    result_for_user = func.print_stats(recent_five)
+    return result_for_user
+
+
+if __name__=="__main__":
+    main()
+
